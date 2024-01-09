aeeb86bec5327956c4710612d46bfb0cc9947d3b
Hhhrui <74440493+Hhhrui@users.noreply.github.com>
2024-01-09 20:09:22 +0800

    Update algorithm.py

--- collector/algorithm.py
+++ collector/algorithm.py
@@ -135,3 +135,83 @@ def prop_schedule_sla2(queues_estimation, ave_delay_vio_estimation, tail_delay_v
         left_pods -= 1
 
     return pod_on_node
+
+"""
+   HAB algorithm
+"""
+def CoeMeasurement(queues_estimation, flow_arrival, flow_route, service_time, pod_on_node, p):
+  """
+    CoeMeasurement is used to learn the coefficient phi_lambda_base calculating pod.
+  """
+    Q_low = 4
+    Q_up = 20
+    threshold = 2
+    phi_lambda_base = 0
+    lambda_base = sum(flow_arrival.values())
+    phi_left = lambda_base
+    phi_right = lambda_base * 10
+
+    while phi_left < phi_right:
+        phi_lambda_base = (phi_left + phi_right)/2
+        for node_name in queues_estimation:
+            pod_on_node[node_name] = math.floor(phi_lambda_base * p[node_name])
+        for node_name in queues_estimation:
+            pod_on_node[node_name] = math.floor(total_capacity * pod_on_node[node_name] / sum(pod_on_node.values()))
+            # We have calculated  pod here, and we should update network and get total queue length and we denote it with Q_lambda_base.
+    if Q_lambda_base == 0:
+        return phi_lambda_base
+    
+    if Q_lambda_base > (Q_up + Q_low) / 2 + threshold:
+        phi_left = phi_lambda_base
+    elif Q_lambda_base < (Q_up + Q_low) / 2 - threshold:
+        phi_right = phi_lambda_base
+    else:
+        return phi_lambda_base
+
+def vs_schedule(flag, queues_estimation, total_capacity, flow_arrival, flow_route, service_time, phi_lambda_base, pod_on_node, p):
+    """
+    vs_schedule is a function to schedule pods for nodes according to the estimated queue length on the nodes
+
+    :param queues_estimation: a dict that represents {node: {flow: ave_queue_length, ..., }} and
+    :param total_capacity: total number of pods we use
+    :param flow_arrival: a dict that include the arrival rate of each node such as {"f1":2, "f2":3}
+    :param flow_route: a dict that represents the nodes that each flow flows through {"f1":["node1", "node2", "node3"], "f2":["node1", "node2", "node4"]}
+    :param service_time: a dict that denotes the processing time for load of each node, can be average time  {"Node1":0.0003, "Node2":0.0002,...}
+
+    :param phi_lambda_base: a parameter that used to calculate pod, is calculated in the function CoeMeasurement
+    :return: a dict that represents {node: pod num}
+
+    let's consider a simple example with two flows and four nodes
+    flow 1 goes through {node1, node2, node3} and flow 2 goes through {node1, node2, node4}
+    queues_estimation = {node1: {f1: 3, f2: 7}, node2: {f1: 2, f2: 3}, node3: {f1: 5}, node4: {f2: 0}}
+    total_capacity = 10
+    flow_arrival = {"f1":2, "f2":3}
+    flow_route = {"f1":["node1", "node2", "node3"], "f2":["node1", "node2", "node4"]}
+    service_time = {"node1":0.0003, "node2":0.0002, "node3":0.0001, "node4":0.001}
+
+    vs_schedule computes and returns the dict pod_on_node
+    pod_on_node = {node1: 2, node2: 2, node3: 2, node4: 2}
+    """
+    lambda_base = sum(flow_arrival.values())
+    lambda_ = sum(flow_arrival.values())
+    pod_on_node = {}
+
+    for node_name in queues_estimation:
+        p[node_name] = 0
+    for node_name in queues_estimation:
+        for flow in flow_route:
+            if node_name in flow_route[flow]:
+                p[node_name] += math.ceil(flow_arrival[flow] * service_time[node_name])
+
+    if flag:
+        phi_lambda_base = CoeMeasurement(queues_estimation, flow_arrival, flow_route, service_time, pod_on_node, p)
+    else:
+        if lambda_ == 0:
+            return pod_on_node
+        for node_name in queues_estimation:
+            if lambda_ is not None and phi_lambda_base is not None and p is not None:
+                pod_on_node[node_name] = max(math.ceil(lambda_ * phi_lambda_base * p[node_name] / lambda_base),1)
+        for node_name in queues_estimation:
+            pod_on_node[node_name] = math.floor(total_capacity * pod_on_node[node_name] / sum(pod_on_node.values()))
+    flag = 0
+    return pod_on_node
